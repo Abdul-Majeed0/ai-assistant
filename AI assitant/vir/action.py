ea6fbbeb7890d7936e8519f941a10f8d430bca2b
Abdul-Majeed0 <135080850+Abdul-Majeed0@users.noreply.github.com>
2025-02-04 08:37:01 +0000

    Add files via upload

---
+++ AI assitant/vir/action.py
@@ -0,0 +1,59 @@
+import text_to_speech
+import speech_to_text
+import datetime 
+import webbrowser
+#import weather
+
+def Action (data):
+    user_data = data.lower()
+
+    if "what is your name" in user_data:
+        text_to_speech.text_to_speech("My name is virtual assistant")
+        return "My name is virtual assistant"
+
+    elif "hello" in user_data or "hey" in user_data or "hi" in user_data:
+        text_to_speech.text_to_speech("hey , sir How can i help you")
+        return "hey , sir How can i help you"
+
+    elif "good morning" in user_data:
+        text_to_speech.text_to_speech("good morning sir ")
+        return "good morning sir "
+
+    elif "time now" in user_data():
+        current_time = datetime.datetime.now()
+        Time = (str)(current_time) + "Hour :" , (str)(current_time.minute) + "Minute"
+        text_to_speech.text_to_speech(Time)
+        return Time
+
+
+    elif "shutdown" in user_data:
+        text_to_speech.text_to_speech("ok sir")
+        return"ok sir"
+
+    elif "play music" in user_data:
+        webbrowser.open("https://spotify.com/")
+        text_to_speech.text_to_speech("spotify.com is now ready for you")
+        return "spotify.com is now ready for you"
+
+    elif "open youtube" in user_data:
+        webbrowser.open("https://youtube.com/")
+        text_to_speech.text_to_speech("youtude.com is now ready for you")
+        return "youtude.com is now ready for you"
+
+    elif "open google" in user_data:
+        webbrowser.open("https://google.com/")
+        text_to_speech.text_to_speech("google.com is now ready for you")
+        return "google.com is now ready for you"
+
+    #elif "weather" in user_data:
+        #ans = weather.weather()
+        #text_to_speech.text_to_speech(ans)
+        #return ans
+
+    else:
+        text_to_speech.text_to_speech("I'm not able to understand")
+        return "I'm not able to understand"
+
+
+
+
---
+++ AI assitant/vir/weather.py
@@ -0,0 +1,15 @@
+#https://www.google.com/search?q=weather+hampton
+# user agent : Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0
+# span   id=wob_tm
+from requests_html import HTMLSession
+import speech_to_text
+
+def weather():
+    s = HTMLSession()
+    query = "hampton"
+    url = f'https://www.google.com/search?q=weather+{query}'
+    r = s.get(url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36' })
+    temp = r.html.find('span#wob_tm', first= True)
+    unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first= True)
+    desc = r.html.find('span#wob_dc', first= True)
+    return temp+" "+ unit+" "+ desc
